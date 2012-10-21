037b3bad8f070ed1487034167d658a73a06760a9
Alexey Shmalko <rasen.dubi@gmail.com>
2012-10-21 18:55:23 +0300

    Initialize translations

---
+++ ts/ua.ts
@@ -0,0 +1,199 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk_UA">
+<context>
+    <name>QVPlayer</name>
+    <message>
+        <location filename="../ui/qvplayer.ui" line="23"/>
+        <source>QVPlayer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/qvplayer.ui" line="43"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/qvplayer.ui" line="61"/>
+        <source>Hello, my master!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/qvplayer.ui" line="120"/>
+        <source>Pre</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/qvplayer.ui" line="136"/>
+        <source>Play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/qvplayer.ui" line="146"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/qvplayer.ui" line="180"/>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/qvplayer.ui" line="209"/>
+        <source>Home</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/qvplayer.ui" line="219"/>
+        <location filename="../src/qvplayer.cpp" line="116"/>
+        <source>Shuffle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/qvplayer.ui" line="229"/>
+        <location filename="../src/qvplayer.cpp" line="118"/>
+        <source>Repeat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="105"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="106"/>
+        <source>&amp;Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="108"/>
+        <source>&amp;Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="109"/>
+        <source>P&amp;lay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="110"/>
+        <source>P&amp;ause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="111"/>
+        <source>&amp;Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="112"/>
+        <source>&amp;Pre</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="113"/>
+        <source>&amp;Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="114"/>
+        <source>Home page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="115"/>
+        <source>Show/&amp;Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="117"/>
+        <source>Clear cookies</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="158"/>
+        <source>Quit the application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="159"/>
+        <source>Mute sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="160"/>
+        <source>Stop music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="161"/>
+        <source>Play music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="162"/>
+        <source>Pause music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="163"/>
+        <source>Play next audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="164"/>
+        <source>Play previous audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="165"/>
+        <source>Search audios</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="166"/>
+        <source>Switch to home playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="167"/>
+        <source>Show/hide player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="168"/>
+        <source>Shuffle playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="169"/>
+        <source>Clear authorization cookies</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="170"/>
+        <source>Repeat current track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="208"/>
+        <source>Main menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="246"/>
+        <source>Friend: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="316"/>
+        <source>My page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qvplayer.cpp" line="348"/>
+        <source>Search: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
